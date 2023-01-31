c3d2d320298943161293330c42ee34a8b7ee04a2
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-01 00:46:09 +0800

    Practice: Build a new object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,22 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+import workstation from "./workstation.js";
+
+const printWorkstation = (workstation) => {
+  var storage = "<ul>";
+  for (const drive of workstation.storage) {
+    storage += `<li>${drive.capacityInGB / 1000}TB ${drive.type}</li>`;
+  }
+  storage += "</ul>";
+  return `
+    <ul>
+      <li>Owner: ${workstation.owner}</li>
+      <li>Memory: ${workstation.memory.capacityInGB}GB ${workstation.memory.type}</li>
+      <li>Storage: ${storage}</li>
+    </ul>
+  `;
+};
+
+document.body.innerHTML = printWorkstation(workstation);
---
+++ Practice/03_07/workstation.js
@@ -0,0 +1,19 @@
+const workstation = {
+  owner: "Jeremy Andrews Zantua",
+  memory: {
+    type: "DDR3",
+    capacityInGB: 16,
+  },
+  storage: [
+    {
+      capacityInGB: 5000,
+      type: "HDD",
+    },
+    {
+      capacityInGB: 500,
+      type: "SSD",
+    },
+  ],
+};
+
+export default workstation;
